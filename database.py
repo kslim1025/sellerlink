e05811fd42108a3f660b247e13e3e34d844fbaea
icmp <jmfield2@mail.usf.edu>
2017-02-22 22:14:56 -0500

    some cleanup, and add config function to handle importing active models

--- database.py
+++ database.py
@@ -22,9 +22,31 @@ def get_or_create(session, model, **kwargs):
     
     instance = session.query(model).filter_by(**kwargs).first()
     if instance:
-        return instance
+        return instance, True
     else:
         params = dict( (k,v) for k,v in kwargs.iteritems() )
         instance = model(**params)
-        return instance
-        
+	session.add( instance )
+        return instance, False
+
+def get_config_db():
+
+	from config import prod as config
+
+	from app import create_app
+
+	from models import thing
+	from models import place, organization
+	from models import geo_coords
+	from models import autodealer
+	from models import historicplace
+	from models import landmarks
+	import models.hotels
+	#import models.restaurant
+
+	application = create_app(config)
+
+	init(application)
+       
+	return db.metadata
+
