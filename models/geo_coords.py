e6204729b9ac5ad252e9c25a8e0a063555a77bd9
icmp <jmfield2@mail.usf.edu>
2017-02-22 22:16:54 -0500

    add columns to postaladdress, and fix geocoords lat/lng precision

--- models/geo_coords.py
+++ models/geo_coords.py
@@ -1,24 +1,32 @@
 
 from database import db
 
-from models.thing import Thing
+from models.thing import Thing, GUID
 
 class PostalAddress(Thing, db.Model):
-    addressCountry = db.Column('addressCountry', db.String(1024))
-    addressLocality = db.Column('addressLocality', db.String(1024))
-    addressRegion = db.Column('addressRegion', db.String(1024))
-    postOfficeBoxNumber = db.Column('postOfficeBoxNumber', db.String(1024))
-    postalCode = db.Column('postalCode', db.String(1024))
+
     streetAddress = db.Column('streetAddress', db.String(1024))
-    
+    streetAddress2 =db.Column('streetAddress2', db.String(1024))
+    streetAddress3 = db.Column('streetAddress3', db.String(1024))
+    postOfficeBoxNumber = db.Column('postOfficeBoxNumber', db.String(1024))
+    addressLocality = db.Column('addressLocality', db.String(1024))
+    postalCode = db.Column('postalCode', db.String(24))
+    addressCountry = db.Column('addressCountry', db.String(1024))
+    addressRegion = db.Column('addressRegion', db.String(1024))
+
+    geo_id = db.Column('geo_id', GUID(), db.ForeignKey('geocoords.guid'))
+    geo = db.relationship('GeoCoords', cascade="all,delete")
+ 
     # ContactPoint
     
 class GeoCoords(Thing, db.Model):
     __table_args__ = (db.Index('uix_1', 'latitude', 'longitude', unique=True),)
     
-    latitude = db.Column('latitude', db.Float)
-    longitude = db.Column('longitude', db.Float)
-    elevation = db.Column('elevation', db.String(1024))
+    latitude = db.Column('latitude', db.Numeric(precision='23,20')) #String(32)) #Float(precision='23,20') ) # 41.8645403460001
+    longitude = db.Column('longitude', db.Numeric(precision='23,20')) #String(32)) #Float(precision='23,20') )
+    elevation = db.Column('elevation', db.Float)
     addressCountry = db.Column('addressCountry', db.String(1024))
-    postalCode = db.Column('postalCode', db.String(1024))
+    postalCode = db.Column('postalCode', db.String(24))
+
+    accuracy = db.Column('accuracy', db.Integer) # Accuracy radius in meters
     
