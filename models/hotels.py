8ea8c7b987a9d0c7ed4c2b8e007a22f213101044
icmp <jmfield2@mail.usf.edu>
2017-02-22 22:19:12 -0500

    cleanups, add hotel, motel, bnb, and resorts

--- models/hotels.py
+++ models/hotels.py
@@ -2,8 +2,10 @@
 from database import db
 from sqlalchemy.schema import ForeignKey
 
-from models import common
+from models.thing import GUID
+from models.place import LocalBusiness
 
+"""
 # Room/Product and RoomOffers for availability calendar
 class RoomOffers(db.Model):
     guid = db.Column('guid', db.String(1024), primary_key=True)
@@ -30,12 +32,14 @@ class Room(db.Model, Thing):
     logo_id = db.Column('logo_id', db.String(1024), ForeignKey('image_object.guid'))
     image_id = db.Column('image_id', db.String(1024), ForeignKey('image_object.guid'))
     containsPlace_id = db.Column('containsPlace_id', db.String(1024), ForeignKey('place.guid'))
-    
+"""
+ 
 #amenityFeature = db.relationship('LocationFeatureSpecification')
 #geo = db.relationship('GeoCoordinates')
 #geo_id = db.Column('geo_id', db.String(1024), ForeignKey('GeoCoordinates.guid'))
 #amenityFeature_id = db.Column('amenityFeature_id', db.String(1024), ForeignKey('LocationFeatureSpecification.guid'))
 
+"""
 class HotelOpeningHours(db.Model):
     guid = db.Column('guid', db.String(1024), primary_key=True)
     hotel_id = db.Column('hotel_id', db.String(1024), ForeignKey('hotel.guid'))
@@ -65,45 +69,48 @@ class HotelImages(db.Model):
     guid = db.Column('guid', db.String(1024), primary_key=True)
     image_id = db.Column('image_id', db.String(1024), ForeignKey('image_object.guid'))
     hotel_id = db.Column('hotel_id', db.String(1024), ForeignKey('hotel.guid'))
+"""
 
-class Hotel(db.Model):
-    guid = db.Column('guid', db.String(1024), primary_key=True)
-    name = db.Column('name', db.String(1024))
-    url = db.Column('url', db.String(1024))
-    sameAs = db.Column('sameAs', db.String(1024)) 
-    address = db.Column('address', db.String(1024))
-    branchCode = db.Column('branchCode', db.String(1024))
-    naics = db.Column('naics', db.String(1024))
+class LodgingBusiness(LocalBusiness):
 
+    # amenityFeature, audience, availableLanguage, checkinTime, checkoutTime, petsAllowed, starRating
+    license_number = db.Column('license_number', db.String(1000))
+    license_expiry = db.Column('license_expiry', db.DateTime())
+
+    last_inspection = db.Column('last_inspection', db.DateTime())
+    status_code = db.Column('status_code', db.String(1000))
+
+    checkinTime = db.Column('checkinTime', db.DateTime())
+    checkoutTime = db.Column('checkoutTime', db.DateTime())
+    rooms = db.Column('rooms', db.Integer())
+
+    __mapper_args__ = {'polymorphic_identity': 'lodgingbusiness'}
+    id = db.Column(GUID(), db.ForeignKey('local_business.id'), primary_key=True)
+
+class Hotel(LodgingBusiness):
+
+    """
     areaServed_id = db.Column('areaServed_id', db.String(1024), ForeignKey('place.guid'))
     areaServed = db.relationship('place', foreign_keys='areaServed_id')
     location_id = db.Column('location_id', db.String(1024), ForeignKey('place.guid'))
     location = db.relationship('place', foreign_keys='location_id')
     containedInPlace_id = db.Column('containedInPlace_id', db.String(1024), ForeignKey('place.guid'))
     containedInPlace = db.relationship('Place', foreign_keys='containedInPlace_id')
+    """
 
-    faxNumber = db.Column('faxNumber', db.String(1024))
-    taxID = db.Column('taxID', db.String(1024))
-    award = db.Column('award', db.String(1024))
-    dissolutionDate = db.Column('dissolutionDate', db.DateTime)
-    legalName = db.Column('legalName', db.String(1024))
-    telephone = db.Column('telephone', db.String(1024))
-    currenciesAccepted = db.Column('currenciesAccepted', db.String(1024))
-    isicV4 = db.Column('isicV4', db.String(1024))
-    description = db.Column('description', db.String(1024))
-    alternateName = db.Column('alternateName', db.String(1024))
-    priceRange = db.Column('priceRange', db.String(1024))
-    paymentAccepted = db.Column('paymentAccepted', db.String(1024))
-    brand_id = db.Column('brand_id', db.String(1024), ForeignKey('organization.guid'))
-    brand = db.relationship('organization')
-    openingHours = db.relationship('HotelOpeningHours', secondary='hotel_opening_hours')
-    owner_id = db.Column('owner_id', db.String(1024), ForeignKey('person.guid'))
-    owner = db.relationship('person', foreign_keys='owner_id')
-    photo_id = db.Column('photo_id', db.String(1024), ForeignKey('image_object.guid'))
-    photo = db.relationship('imageobject', foreign_keys='photo_id')
-    logo_id = db.Column('logo_id', db.String(1024), ForeignKey('image_object.guid'))
-    logo = db.relationship('imageobject', foreign_keys='logo_id')
-    # event, review
-    offers = db.relationship('offers', secondary=HotelOffers)
+    __mapper_args__ = {'polymorphic_identity': 'hotel'}
+    id = db.Column(GUID(), db.ForeignKey('lodging_business.id'), primary_key=True)
+
+class Motel(LodgingBusiness):
+    __mapper_args__ = {'polymorphic_identity': 'motel'}
+    id = db.Column(GUID(), db.ForeignKey('lodging_business.id'), primary_key=True)
+
+class BedAndBreakfast(LodgingBusiness):
+    __mapper_args__ = {'polymorphic_identity': 'bedandbreakfast'}
+    id = db.Column(GUID(), db.ForeignKey('lodging_business.id'), primary_key=True)
+
+class Resort(LodgingBusiness):
+    __mapper_args__ = {'polymorphic_identity': 'resort'}
+    id = db.Column(GUID(), db.ForeignKey('lodging_business.id'), primary_key=True)
 
 
